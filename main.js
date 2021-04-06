96d922a594bf1b74dc27113cb1eb5dd36ab9638f
吳俊緯 <wcw850710@gmail.com>
2021-04-06 16:07:55 +0800

    chore: 拆分邏輯到各自檔案

--- main.js
+++ main.js
@@ -1,28 +1,3 @@
-import { fromEvent, interval, Subject } from 'rxjs'
-import { map, take, takeUntil } from 'rxjs/operators'
+import { innerApp } from './src/innerApp'
+import { simpleDrag } from './src/simple-drag'
 import './style.css'
-import { concatAll } from 'rxjs/src/internal/operators/concatAll'
-
-const app = document.querySelector('#app')
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
-
-
-const dragDom = document.createElement('div')
-dragDom.id = 'drag'
-app.append(dragDom)
-
-const mousedown$ = fromEvent(dragDom, 'mousedown')
-const mousemove$ = fromEvent(document, 'mousemove')
-const mouseup$ = fromEvent(document, 'mouseup')
-
-mousedown$.pipe(
-  map(() => mousemove$.pipe(takeUntil(mouseup$))),
-  concatAll(),
-  map(e => ({x: e.clientX, y: e.clientY})),
-).subscribe(pos => {
-  dragDom.style.left = pos.x + 'px'
-  dragDom.style.top = pos.y + 'px'
-})
