6af204ef2171908d63d453be89b6abea8ff6cf7c
吳俊緯 <wcw850710@gmail.com>
2021-04-06 20:12:52 +0800

    feat: 簡單的 fetch 圖片功能

---
+++ src/fetch-button.js
@@ -0,0 +1,44 @@
+import { app } from './helper'
+import { defer, fromEvent } from 'rxjs'
+import {
+  combineLatest,
+  concatMap,
+  filter,
+  map,
+  mapTo,
+  merge,
+  startWith,
+  takeUntil,
+  tap,
+  withLatestFrom
+} from 'rxjs/operators'
+import { concatAll } from 'rxjs/src/internal/operators/concatAll'
+
+const img = document.createElement('img')
+const fetchBtn = document.createElement('button')
+const wrap = document.createElement('div')
+fetchBtn.innerText = `fetch~!`
+wrap.append(fetchBtn)
+wrap.append(img)
+app.append(wrap)
+
+const fetchDogUrl = () => new Promise(resolve => {
+  setTimeout(async () => {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random').then(res => res.json())
+    resolve(response.message)
+  }, 1000)
+})
+
+const click$ = fromEvent(fetchBtn, 'click')
+  .pipe(
+    filter(e => e.target.innerText !== `wait...`),
+    tap(e => e.target.innerText = `wait...`),
+    concatMap(fetchDogUrl),
+  )
+
+click$.subscribe(url => {
+  fetchBtn.innerText = `fetch~!`
+  img.src = url
+})
+
+export const fetchButton = null
