fc529d33c078c2d20914e9af80c260545e96e752
吳俊緯 <wcw850710@gmail.com>
2021-04-06 16:08:25 +0800

    feat: 新增拖曳指標位置不變

---
+++ src/simple-drag.js
@@ -0,0 +1,27 @@
+import { fromEvent } from 'rxjs'
+import { map, takeUntil, withLatestFrom } from 'rxjs/operators'
+import { concatAll } from 'rxjs/src/internal/operators/concatAll'
+import { app } from './helper'
+
+const dragDom = document.createElement('div')
+dragDom.id = 'drag'
+app.append(dragDom)
+
+const mousedown$ = fromEvent(dragDom, 'mousedown')
+const mousemove$ = fromEvent(document, 'mousemove')
+const mouseup$ = fromEvent(document, 'mouseup')
+
+mousedown$.pipe(
+  map(() => mousemove$.pipe(takeUntil(mouseup$))),
+  concatAll(),
+  withLatestFrom(mousedown$, (move, down) => ({
+    x: move.clientX - down.offsetX,
+    y: move.clientY - down.offsetY,
+  })),
+  // map(e => ({x: e.clientX, y: e.clientY})),
+).subscribe(pos => {
+  dragDom.style.left = pos.x + 'px'
+  dragDom.style.top = pos.y + 'px'
+})
+
+export const simpleDrag = null
